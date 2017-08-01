7ccb4ce96f58e0dbd27e91625472d130502c333c
Rafael Schloming <rhs@alum.mit.edu>
2017-08-01 16:11:40 -0400

    updated tasks service to use mongo

--- tasks/app.py
+++ tasks/app.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 
 import time
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo, ObjectId
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://todo-db-0.todo-db,todo-db-1.todo-db,todo-db-2.todo-db:27017"
+app.config["MONGO_DBNAME"] = "tasks"
+mongo = PyMongo(app)
 
 START = time.time()
 
@@ -12,9 +17,23 @@ def elapsed():
     hours, minutes = divmod(minutes, 60)
     return "%d:%02d:%02d" % (hours, minutes, seconds)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def root():
-    return "Hello Tasks! (up %s)\n" % elapsed()
+    result = mongo.db.tasks.find(projection={"_id": False})
+    return jsonify(list(result))
+
+@app.route('/<tag>', methods=["POST"])
+def add(tag):
+    result = mongo.db.tasks.insert_one({"tag": tag, "task": request.json})
+    return jsonify({"status": "ok", "id": str(result.inserted_id)})
+
+@app.route('/<tag>', methods=["GET"])
+def get(tag):
+    result = list(mongo.db.tasks.find({"tag": tag}, projection={"_id": False}))
+    if result:
+        return jsonify(result[0])
+    else:
+        return ('Tag not found: %s\n' % tag, 404)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
